a640464def219ed426a645b032f87f9db4d3e8f8
AlwinaO <aoyewole2586@gmail.com>
2019-12-26 21:21:00 -0500

    add seed data and seed file to view in browser

--- cptips-backend/db/seeds.rb
+++ cptips-backend/db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+tips = Tip.create([
+    {title: "Time on the basics", author: "@franciscormrz", content: "You should put time on the basics. If you dive too deep too fast, you may be overlooking important concepts", tip_url: "https://twitter.com/franciscormrz/status/1207475481928273920?s=20"},
+
+{title: "Relate new content with prior knowledge", author: "@ThankUBayesGod", content: "Try to related things like loops, objects, etc to mathematics to give your new knowledge prior knowledge to build on", tip_url: "https://twitter.com/ThankUBayesGod/status/1207702900576702465?s=20"},
+
+{title: "Try different things as a newbie", author: "@landedstar", content: "Being new is a time to be broad. Try out a hundred different languages, IDEs, frameworks, DBs, OSes, methodologies, etc. Don’t worry about digging deep until you find what sparks joy.", tip_url: "https://twitter.com/landedstar/status/1207397092337102849?s=20"},
+
+{title: "Pseudo Code", author: "@the_real_stacie", content: "Pseudo code and a good plan before any code… No matter what language you're working in pseudo code is a solid first step in solving a prob. If you jump straight to code then you limit yourself to what you know and can get stuck trying to make something work based on syntax you are familiar with.", tip_url: "https://twitter.com/the_real_stacie/status/1207366754164363264?s=20"},
+
+{title: "Read documentation", author: "@PhilBillingsby", content: "Read as much documentation as you can. Plan your code step by step. Pseudocode. Don’t be intimidated by it all. It will make sense at some point!", tip_url: "https://twitter.com/PhilBillingsby/status/1207407594425507844?s=20"},
+
+{title: "Grasp the fundamentals", author: "@richardsession", content: "Try to really grasp the concepts behind what you can do with a programming language and how you can solve problems with it. Most of the popular languages are 85% similar but they just look a little different. If you can grasp the fundamentals then being a polyglot is a lot easier", tip_url: "https://twitter.com/richardsession/status/1207364028970020864?s=20"},
+
+{title: "Comment your code and find a good IDE", author: "@gobias_infosec", content: "Find an IDE you like that analyzes your code, keeps it clean looking and lets you refactor entire libraries at once I mostly program in python and use pycharm since its free. Additionally, comment your code. You will forget what things do even if you wrote them.", tip_url: "https://twitter.com/gobias_infosec/status/1207364292623908865?s=20"},
+
+{title: "Build things", author: "@javierromario", content: "Stop learning and build things, you will pick stuff up on the way! Get away from tutorial purgatory.", tip_url: "https://twitter.com/javierromario/status/1207645903202508801?s=20"},
+
+{title: "Practice, practice, practice", author: "@PRPGWorldWizard", content: "Practice and a gradual deep dive approach to learning by doing served me well learning the Java programming language. I know have my own game engine thanks to the dedication I put in!", tip_url: "https://twitter.com/PRPGWorldWizard/status/1207366374873534464?s=20"},
+
+{title: "Build, build, build", author: "@eatOkraTheApp", content: "Build build build, find something you have a passion for and build it. More importantly use it. All the knowledge needed will fall into place. Don't fall into the tutorial trap.", tip_url: "https://twitter.com/eatOkraTheApp/status/1207712561283698694?s=20"},
+
+{title: "How do you learn", author: "@RideWithDaMac", content: "It’s not about what you learn when starting off but HOW you learn. Everybody has their own way of grasping information, you just have to find out which way suits you best. Consistency is also key too, it’s tough but trust me every dev started from zero at one point.", tip_url: "https://twitter.com/RideWithDaMac/status/1207364278317125632?s=20"},
+
+{title: "Break down steps with a scheduling tool", author: "@unikornintech", content: "Use a scheduling tool like Airtable to break down the steps", tip_url: "https://twitter.com/unikornintech/status/1207628701346795521?s=20"}
+
+])
