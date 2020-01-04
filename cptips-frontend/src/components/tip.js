7b15c894f750fad5ad93af09dd96eb43d79ec1d1
AlwinaO <aoyewole2586@gmail.com>
2020-01-04 18:37:46 -0500

    add div class tip to wrap each tip with info

--- cptips-frontend/src/components/tip.js
+++ cptips-frontend/src/components/tip.js
@@ -10,11 +10,38 @@ class Tip {
     }
 
     renderTip() {
-        return `<h1>${this.title}</h1>
-                <p>${this.content}</p>
-                <p>${this.author}</p>
-                <p>${this.tipUrl}</p>`
+        return `<div class="tip">
+                    <h1>${this.title}</h1>
+                    <p>${this.content}</p>
+                    <p>${this.author}</p>
+                    <p>${this.tipUrl}</p>
+                </div>`
     }
+
+
+    // renderTip() {
+    //     const tipDiv = document.querySelector(".tip");
+
+    //     const tipTitleH1 = document.createElement('h1');
+    //     tipTitleH1.innerHTML = this.title;
+    //     tipDiv.appendChild(tipTitleH1);
+
+    //     const tipContentP = document.createElement('p');
+    //     tipContentP.innerHTML = this.content;
+    //     tipTitleH1.appendChild(tipContentP);
+
+    //     const tipAuthorP = document.createElement('p');
+    //     tipAuthorP.innerHTML = this.author;
+    //     tipTitleH1.appendChild(tipAuthorP);
+
+    //     const tipUrlP = document.createElement('p');
+    //     tipUrlP.innerHTML = this.tipUrl;
+    //     tipTitleH1.appendChild(tipUrlP);
+
+    //     return document.getElementById("tips-container") += tipDiv;
+        
+
+    // }
 
     // renderTipFront() {
     //     return `<h1>${this.title}</h1>`
