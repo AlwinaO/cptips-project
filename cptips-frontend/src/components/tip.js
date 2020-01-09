e2e0fb82d651859dcf68cb7ded121ae8b18c16c7
AlwinaO <aoyewole2586@gmail.com>
2020-01-08 20:20:34 -0500

    add html edit form to open as a modal

--- cptips-frontend/src/components/tip.js
+++ cptips-frontend/src/components/tip.js
@@ -15,11 +15,39 @@ class Tip {
                     <p>${this.content}</p>
                     <p>${this.author}</p>
                     <p><a href="${this.tipUrl}" target="_blank">Link to Tip</a></p>
-                    <button data-id=${this.id}>Edit Tip</button>
+                    <button class="button-modal" data-id=${this.id}>Edit Tip</button>
                 </div>`
     }
 
+   renderEditTipForm() {
+       return `
+            <div class="modal">
+                <div class="modal-content">
+                    <span class="close-button">close</span>
+                    <form data-id="${this.id}">
+                        <label>Title:</label>
+                        <input type="text" id="title" name="title" value="${this.title}">
+
+                        <label>Content:</label>
+                        <textarea id="content" name="content" cols="30" rows="5">${this.content}</textarea>
+
+                        <label>Author:</label>
+                        <input type="text" id="author" name="author" value="${this.author}">
+
+                        <label>Link to Tip:</label>
+                        <input type="url" id="tip-url" name="tip-url" value="${this.tipUrl}">
+
+                        <input type="submit" value="Create Tip">
+                    </form>
+                </div>
+
+            </div>
+       `
+   }
+
 }
+
+
     // renderTip() {
     //     const tipDiv = document.querySelector(".tip");
 
